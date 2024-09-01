18968f2c92ca129b36da61dfd8ce3518a26f9b3d
BobKing <313795639@qq.com>
2024-09-01 16:31:32 +0800

    datastructure-training: add and update some files

--- Leetcode_Learning/src/com/bobking/leetcode/training/Number253.java
+++ Leetcode_Learning/src/com/bobking/leetcode/training/Number253.java
@@ -1,9 +1,29 @@
 package com.bobking.leetcode.training;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 /**
  * @Date: 2024/3/1 8:40
  * @Author: BobKing
  * @Description:
  */
 public class Number253 {
+
+    public int minMeetingRooms(int[][] intervals) {
+
+        if (intervals.length == 0)
+            return 0;
+
+        PriorityQueue<Integer> allocator = new PriorityQueue<Integer>(intervals.length, (a, b) -> a - b);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        allocator.add(intervals[0][1]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= allocator.peek())
+                allocator.poll();
+            allocator.add(intervals[i][1]);
+        }
+        return allocator.size();
+    }
 }
