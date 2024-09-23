8c81658875dbc35c4f9fe06b6a123b3ed78983f0
BobKing <313795639@qq.com>
2024-09-24 00:10:00 +0800

    datastructure-training: add and update some files

--- Leetcode_Learning/src/com/bobking/leetcode/training/Number64.java
+++ Leetcode_Learning/src/com/bobking/leetcode/training/Number64.java
@@ -20,7 +20,7 @@ public class Number64 {
 
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[0].length; j++)
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + dp[i][j];
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
         }
 
         return dp[dp.length - 1][dp[0].length - 1];
