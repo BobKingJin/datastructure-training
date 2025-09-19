f760dec5c8359027211d713e72bc6d3399a10e72
BobKing <313795639@qq.com>
2025-09-19 11:12:12 +0800

    datastructure-training: add and update some files

--- Leetcode_Learning/src/com/bobking/leetcode/training/Number136.java
+++ Leetcode_Learning/src/com/bobking/leetcode/training/Number136.java
@@ -5,14 +5,14 @@ public class Number136 {
     // 参考：程序猿代码指南P356
     public int singleNumber(int[] nums) {
 
-        if (nums == null || nums.length == 0)
+        if (nums == null || nums.length == 0) {
             return 0;
+        }
 
         int res = 0;
-
-        for (int num : nums)
+        for (int num : nums) {
             res ^= num;
-
+        }
         return res;
     }
 }
